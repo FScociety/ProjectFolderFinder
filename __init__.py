7c9eaa03628ab7a4c1f5025111b2a6af6951d6b9
soeren <soeren@DESKTOP-NCU3OCN>
2024-03-03 23:12:56 +0100

    first commit

---
+++ __init__.py
@@ -0,0 +1,49 @@
+bl_info = {
+    "name": "Project Folder Opener",
+    "blender": (2, 80, 0),
+    "category": "System",
+    "description": "Quickly view the current file path in a file explorer window",
+    "author": "Sören Schmidt-Clausen",
+    "version": (1, 0),
+    "location": "File > Project Folder",
+}
+
+import bpy
+import os
+import subprocess
+from bpy.types import Operator
+
+class OPEN_BLEND_FOLDER_OT_open_file_folder(Operator):
+    bl_idname = "file.open_blend_folder"
+    bl_label = "Project Folder"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    filename_ext = ""
+
+    def execute(self, context):
+        filepath = bpy.data.filepath
+        if filepath:
+            folder_path = os.path.dirname(filepath)
+            
+            if os.name == 'nt':  # Windows
+                os.startfile(folder_path)
+            elif os.name == 'posix':  # macOS and Linux
+                subprocess.Popen(['open' if sys.platform == 'darwin' else 'xdg-open', folder_path])
+            # self.report({'INFO'}, "Folder opened: " + folder_path)
+        else:
+            self.report({'WARNING'}, "Save the blend file first!")
+        return {'FINISHED'}
+
+def menu_func(self, context):
+    self.layout.operator("file.open_blend_folder", icon="FOLDER_REDIRECT")
+
+def register():
+    bpy.utils.register_class(OPEN_BLEND_FOLDER_OT_open_file_folder)
+    bpy.types.TOPBAR_MT_file.append(menu_func)
+
+def unregister():
+    bpy.types.TOPBAR_MT_file.remove(menu_func)
+    bpy.utils.unregister_class(OPEN_BLEND_FOLDER_OT_open_file_folder)
+
+if __name__ == "__main__":
+    register()
